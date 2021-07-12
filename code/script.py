e85c463fd7dab6f0bc35681ec7500c78e69cbac7
Marcin Kruk <marcinkruk@astripolska.pl>
2021-07-12 22:21:14 +0200

    Add data and script hanling the data

---
+++ code/script.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import numpy
+import pandas
+import pywt
+
+# Import input files
+dollar_data=pandas.read_csv("./input_data/TWEXMMTH.csv")
+oil_data=pandas.read_csv("./input_data/oil.csv")
+
+# Create surrogate by shuffling
+
+# Calculate log returns
+dollar_data["log_return"]=numpy.log(dollar_data.TWEXMMTH) - numpy.log(dollar_data.TWEXMMTH.shift(1))
+oil_data["log_return"]=numpy.log(oil_data.usd) - numpy.log(oil_data.usd.shift(1))
+
+print(dollar_data)
+print(oil_data)
+
+# Wavelet transform
+#wavelet_basis = pywt.Wavelet('db4')
