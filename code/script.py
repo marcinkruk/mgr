f7d2d084c82c9cf0a9dfea0fefa6396ed42424fe
Marcin Kruk <marcinkruk@astripolska.pl>
2021-07-13 22:26:39 +0200

    Add array shuffling

--- code/script.py
+++ code/script.py
@@ -9,6 +9,9 @@ dollar_data=pandas.read_csv("./input_data/TWEXMMTH.csv")
 oil_data=pandas.read_csv("./input_data/oil.csv")
 
 # Create surrogate by shuffling
+if False: # TODO: have it shuffle conditionally
+    numpy.random.shuffle(dollar_data.TWEXMMTH.to_numpy())
+    numpy.random.shuffle(oil_data.usd.to_numpy())
 
 # Calculate log returns
 dollar_data["log_return"]=numpy.log(dollar_data.TWEXMMTH) - numpy.log(dollar_data.TWEXMMTH.shift(1))
