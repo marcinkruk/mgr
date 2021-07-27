9dbfc2cc9de4c1c2e30066b2ac912cbfc26b02f5
Marcin Kruk <marcinkruk@astripolska.pl>
2021-07-27 22:36:15 +0200

    Refactoring of the script

--- code/script.py
+++ code/script.py
@@ -3,6 +3,8 @@
 import numpy
 import pandas
 import pywt
+import subprocess
+import os
 
 def filter_wavelets(arr, n):
 	tmp=arr.copy()
@@ -11,74 +13,73 @@ def filter_wavelets(arr, n):
 			tmp[i]=numpy.zeros_like(tmp[i])
 	return tmp
 
+def shuffle_input(dollar, oil):
+    numpy.random.shuffle(dollar.TWEXMMTH.to_numpy())
+    numpy.random.shuffle(oil.usd.to_numpy())
+
+def analyze(arr1, arr2):
+    print('counter is: '+str(counter))
+    dollar=arr1.copy()
+    oil=arr2.copy()
+
+    # Calculate log returns
+    dollar["log_return"]=numpy.log(dollar.TWEXMMTH) - numpy.log(dollar.TWEXMMTH.shift(1))
+    oil["log_return"]=numpy.log(oil.usd) - numpy.log(oil.usd.shift(1))
+
+    # Wavelet transform
+    oil_wavedec=pywt.wavedec(oil.log_return[1:], 'db4', mode='zero')
+    dollar_wavedec=pywt.wavedec(dollar.log_return[1:], 'db4', mode='zero')
+
+    # Multi-resolution analysis
+    oil_wr=pandas.DataFrame()
+    oil_wr['0']=pywt.waverec(filter_wavelets(oil_wavedec,0), 'db4', mode='zero')
+    oil_wr['1']=pywt.waverec(filter_wavelets(oil_wavedec,1), 'db4', mode='zero')
+    oil_wr['2']=pywt.waverec(filter_wavelets(oil_wavedec,2), 'db4', mode='zero')
+    oil_wr['3']=pywt.waverec(filter_wavelets(oil_wavedec,3), 'db4', mode='zero')
+    oil_wr['4']=pywt.waverec(filter_wavelets(oil_wavedec,4), 'db4', mode='zero')
+    oil_wr['5']=pywt.waverec(filter_wavelets(oil_wavedec,5), 'db4', mode='zero')
+    oil_wr['6']=pywt.waverec(filter_wavelets(oil_wavedec,6), 'db4', mode='zero')
+
+    dollar_wr=pandas.DataFrame()
+    dollar_wr['0']=pywt.waverec(filter_wavelets(dollar_wavedec,0), 'db4', mode='zero')
+    dollar_wr['1']=pywt.waverec(filter_wavelets(dollar_wavedec,1), 'db4', mode='zero')
+    dollar_wr['2']=pywt.waverec(filter_wavelets(dollar_wavedec,2), 'db4', mode='zero')
+    dollar_wr['3']=pywt.waverec(filter_wavelets(dollar_wavedec,3), 'db4', mode='zero')
+    dollar_wr['4']=pywt.waverec(filter_wavelets(dollar_wavedec,4), 'db4', mode='zero')
+    dollar_wr['5']=pywt.waverec(filter_wavelets(dollar_wavedec,5), 'db4', mode='zero')
+    dollar_wr['6']=pywt.waverec(filter_wavelets(dollar_wavedec,6), 'db4', mode='zero')
+
+    outputdir='./output/'+format(counter, '05d')
+    os.mkdir(outputdir)
+    for col in oil_wr.iteritems():
+        c=col[0]
+        oil_wr[c].to_csv(outputdir+'/oil_wr_'+c, header=None, index=None)
+        dollar_wr[c].to_csv(outputdir+'/dollar_wr_'+c, header=None, index=None)
+
+    # Call Diks-Panchenko code
+    # Parse output
+
+    # Call linear GC
+    # Parse output
+
+    # Cleanup
+    del oil
+    del dollar
+    del oil_wavedec
+    del dollar_wavedec
+    del oil_wr
+    del dollar_wr
+
+    return None
+
 # Import input files
 dollar_data=pandas.read_csv("./input_data/TWEXMMTH.csv")
 oil_data=pandas.read_csv("./input_data/oil.csv")
 
-# Create surrogate by shuffling
-if False: # TODO: have it shuffle conditionally
-    numpy.random.shuffle(dollar_data.TWEXMMTH.to_numpy())
-    numpy.random.shuffle(oil_data.usd.to_numpy())
+counter=0
 
-# Calculate log returns
-dollar_data["log_return"]=numpy.log(dollar_data.TWEXMMTH) - numpy.log(dollar_data.TWEXMMTH.shift(1))
-oil_data["log_return"]=numpy.log(oil_data.usd) - numpy.log(oil_data.usd.shift(1))
-
-#print(dollar_data)
-#print(oil_data)
-
-# Wavelet transform
-oil_wavedec=pywt.wavedec(oil_data.log_return[1:], 'db4', mode='zero')
-dollar_wavedec=pywt.wavedec(dollar_data.log_return[1:], 'db4', mode='zero')
-
-#print(oil_wavedec)
-#print(dollar_wavedec)
-
-# Multi-resolution analysis
-oil_wr_0=pywt.waverec(filter_wavelets(oil_wavedec,0), 'db4', mode='zero')
-oil_wr_1=pywt.waverec(filter_wavelets(oil_wavedec,1), 'db4', mode='zero')
-oil_wr_2=pywt.waverec(filter_wavelets(oil_wavedec,2), 'db4', mode='zero')
-oil_wr_3=pywt.waverec(filter_wavelets(oil_wavedec,3), 'db4', mode='zero')
-oil_wr_4=pywt.waverec(filter_wavelets(oil_wavedec,4), 'db4', mode='zero')
-oil_wr_5=pywt.waverec(filter_wavelets(oil_wavedec,5), 'db4', mode='zero')
-oil_wr_6=pywt.waverec(filter_wavelets(oil_wavedec,6), 'db4', mode='zero')
-
-#print(oil_wr_0)
-#print(oil_wr_1)
-#print(oil_wr_2)
-#print(oil_wr_3)
-#print(oil_wr_4)
-#print(oil_wr_5)
-#print(oil_wr_6)
-
-dollar_wr_0=pywt.waverec(filter_wavelets(dollar_wavedec,0), 'db4', mode='zero')
-dollar_wr_1=pywt.waverec(filter_wavelets(dollar_wavedec,1), 'db4', mode='zero')
-dollar_wr_2=pywt.waverec(filter_wavelets(dollar_wavedec,2), 'db4', mode='zero')
-dollar_wr_3=pywt.waverec(filter_wavelets(dollar_wavedec,3), 'db4', mode='zero')
-dollar_wr_4=pywt.waverec(filter_wavelets(dollar_wavedec,4), 'db4', mode='zero')
-dollar_wr_5=pywt.waverec(filter_wavelets(dollar_wavedec,5), 'db4', mode='zero')
-dollar_wr_6=pywt.waverec(filter_wavelets(dollar_wavedec,6), 'db4', mode='zero')
-
-# Represent as pandas DataFrame
-oil_wr=pandas.DataFrame()
-oil_wr['0']=oil_wr_0
-oil_wr['1']=oil_wr_1
-oil_wr['2']=oil_wr_2
-oil_wr['3']=oil_wr_3
-oil_wr['4']=oil_wr_4
-oil_wr['5']=oil_wr_5
-oil_wr['6']=oil_wr_6
-
-dollar_wr=pandas.DataFrame()
-dollar_wr['0']=dollar_wr_0
-dollar_wr['1']=dollar_wr_1
-dollar_wr['2']=dollar_wr_2
-dollar_wr['3']=dollar_wr_3
-dollar_wr['4']=dollar_wr_4
-dollar_wr['5']=dollar_wr_5
-dollar_wr['6']=dollar_wr_6
-
-# Output to csv
-dollar_wr.to_csv('./output/dollar_wr.csv')
-oil_wr.to_csv('./output/oil_wr.csv')
+for i in range(10):
+    analyze(dollar_data, oil_data)
+    shuffle_input(dollar_data, oil_data)
+    counter+=1
 
